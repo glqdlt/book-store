e28d42424448846f025b598935903a3764352df8
glqdlt <dlfdnd0725@gmail.com>
2019-01-22 23:12:04 +0900

    slack message bot module 을 추가

---
+++ slack-message-bot/src/main/java/com/glqdlt/bookstore/slackwhisperbot/SlackWhisperBotApplication.java
@@ -0,0 +1,14 @@
+package com.glqdlt.bookstore.slackwhisperbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackWhisperBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackWhisperBotApplication.class, args);
+	}
+
+}
+
---
+++ slack-message-bot/src/test/java/com/glqdlt/bookstore/slackwhisperbot/SlackWhisperBotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.glqdlt.bookstore.slackwhisperbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlackWhisperBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
