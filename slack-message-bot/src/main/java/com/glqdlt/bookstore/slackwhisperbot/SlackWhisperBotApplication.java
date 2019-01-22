0790b64770a3052c0d39978c3241be1e9c044b34
glqdlt <dlfdnd0725@gmail.com>
2019-01-22 23:12:26 +0900

    주석

--- slack-message-bot/src/main/java/com/glqdlt/bookstore/slackwhisperbot/SlackWhisperBotApplication.java
+++ slack-message-bot/src/main/java/com/glqdlt/bookstore/slackwhisperbot/SlackWhisperBotApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SlackWhisperBotApplication {
-
+//	https://api.slack.com/methods/chat.postMessage
 	public static void main(String[] args) {
 		SpringApplication.run(SlackWhisperBotApplication.class, args);
 	}
