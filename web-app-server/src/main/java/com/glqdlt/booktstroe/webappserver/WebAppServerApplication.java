ae5c257f09dcf57e6f2a26ace73372928299ff23
glqdlt <dlfdnd0725@gmail.com>
2019-01-26 15:11:38 +0900

    web-app added

---
+++ web-app-server/src/main/java/com/glqdlt/booktstroe/webappserver/WebAppServerApplication.java
@@ -0,0 +1,14 @@
+package com.glqdlt.booktstroe.webappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppServerApplication.class, args);
+	}
+
+}
+
