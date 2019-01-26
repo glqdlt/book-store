98cf99d1bb2601e41075ae13cbc10e56d2a5abe0
glqdlt <dlfdnd0725@gmail.com>
2019-01-26 15:20:08 +0900

    mysql dependency added

--- web-app-server/src/test/java/com/glqdlt/booktstroe/webappserver/WebAppServerApplicationTests.java
+++ web-app-server/src/test/java/com/glqdlt/booktstroe/webappserver/WebAppServerApplicationTests.java
@@ -2,12 +2,20 @@ package com.glqdlt.booktstroe.webappserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+
+@ActiveProfiles("prod")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebAppServerApplicationTests {
+
+	@Autowired
+	private EntityManager entityManager;
 
 	@Test
 	public void contextLoads() {
