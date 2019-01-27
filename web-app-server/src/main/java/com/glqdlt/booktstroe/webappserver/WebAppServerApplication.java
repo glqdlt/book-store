4988fa9952e662b9e8076105dce42bff024d038c
glqdlt <dlfdnd0725@gmail.com>
2019-01-27 16:02:48 +0900

    ddl config 수정

--- web-app-server/src/main/java/com/glqdlt/booktstroe/webappserver/WebAppServerApplication.java
+++ web-app-server/src/main/java/com/glqdlt/booktstroe/webappserver/WebAppServerApplication.java
@@ -3,7 +3,9 @@ package com.glqdlt.booktstroe.webappserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @ComponentScan(basePackages = "com.glqdlt.bookstore.**")
 @SpringBootApplication
 public class WebAppServerApplication {
